b21648777c1e3b5398e579fe4eb6a49b9d9f91cb
odlazek <60104777+odlazek@users.noreply.github.com>
2022-06-16 14:09:06 +0200

    init

---
+++ assets/mobilocale/sl-SI.ts
@@ -0,0 +1,38 @@
+import { MbscLocale } from '@mobiscroll/angular';
+
+const LocaleSlSI: MbscLocale = {
+  setText: 'Potrdi',
+  cancelText: 'Prekliči',
+  clearText: 'Izbriši',
+  selectedText: '{count} izbrano',
+  // Datetime component
+  dateFormat: 'DD.MM.YYYY',
+  dateFormatLong: 'DDD, D.MMM.YYYY',
+  dayNames: ['Nedelja', 'Ponedeljek', 'Torek', 'Sreda', 'Četrtek', 'Petek', 'Sobota'],
+  dayNamesShort: ['Ned', 'Pon', 'Tor', 'Sre', 'Čet', 'Pet', 'Sob'],
+  dayNamesMin: ['N', 'P', 'T', 'S', 'Č', 'P', 'S'],
+  todayText: 'Danes',
+  monthNames: ['Januar', 'Februar', 'Marec', 'April', 'Maj', 'Junij', 'Julij', 'Avgust', 'September', 'Okotober', 'November', 'December'],
+  monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+  timeFormat: 'HH:mm',
+  nowText: 'Sedaj',
+  pmText: 'pm',
+  amText: 'am',
+  // Calendar component
+  firstDay: 1,
+  dateText: 'Datum',
+  timeText: 'Čas',
+  prevMonthText: 'Prejšnji mesec',
+  nextMonthText: 'Naslednji mesec',
+  prevYearText: 'Prejšnje leto',
+  nextYearText: 'Naslednje leto',
+  closeText: 'Zapri',
+  eventText: 'Dogodek',
+  eventsText: 'Dogodki',
+  allDayText: 'Cel dan',
+  noEventsText: 'Ni dogodkov',
+  moreEventsText: '{count} Več',
+};
+
+
+export default LocaleSlSI;
